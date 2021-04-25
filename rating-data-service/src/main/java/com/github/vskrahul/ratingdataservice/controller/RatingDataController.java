501349a1250ac36b80f62c2a0395001ea094f18a
Rahul Vishvakarma <C-Rahul.Vishvakarma@charter.com>
2021-04-26 02:44:00 +0530

    update

--- rating-data-service/src/main/java/com/github/vskrahul/ratingdataservice/controller/RatingDataController.java
+++ rating-data-service/src/main/java/com/github/vskrahul/ratingdataservice/controller/RatingDataController.java
@@ -5,6 +5,7 @@ import com.github.vskrahul.ratingdataservice.model.Rating;
 import com.github.vskrahul.ratingdataservice.model.UserRating;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class RatingDataController {
     private RatingDataClient ratingDataClient;
 
     @Autowired
-    public RatingDataController(RatingDataClient ratingDataClient) {
+    public RatingDataController(RatingDataClient ratingDataClient,
+                                @Value("${config}") String config) {
         this.ratingDataClient = ratingDataClient;
     }
 
